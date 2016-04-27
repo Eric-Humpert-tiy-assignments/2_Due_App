d1586b1add7366b5e33f9d49c96b9a19cb9e30f1
Eric Humpert <EricMac@ricks-mbp.apollosrc.root>
2016-04-27 12:03:12 -0500

    updated styling and added some functionality to the page

--- public/script.js
+++ public/script.js
@@ -1,15 +1,28 @@
 'use strict';
-if (this.AppName === undefined) this.AppName = {};
+if (this.toDoApp === undefined) this.toDoApp = {};
 
 (function(context) {
 
-  function start() {
+    function checkIfChecked(task) {
+      console.log(task);
+        if (task.target.checked === true) {
+            console.log("It's checked!");
+        }
+    }
 
-    //Call your code here
-    console.log('starting!', context);
+    function start() {
+        var taskCollector = document.querySelector('#task-adder');
+        var taskList = document.querySelector('.toDoList');
+        var checkBoxList = document.querySelectorAll('.to-do-check');
+        //Call your code here
+        console.log('starting!', context);
 
-  }
+        for (var i = 0; i < checkBoxList.length; i++) {
+            var checkBox = checkBoxList[i];
+            checkBox.addEventListener('change', checkIfChecked);
+        }
+    }
 
-  window.AppName.start = start;
+    window.toDoApp.start = start;
 
-})(window.AppName);
+})(window.toDoApp);
